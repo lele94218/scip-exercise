4412e439667449f6f0dce7848c3654f356885c39
lele94218 <terry.trxue@gmail.com>
2016-12-15 18:23:32 -0500

    1.7

--- 1-7.lisp
+++ 1-7.lisp
@@ -1,18 +1,33 @@
-(define (good-enough? guess x) 
-  (< (abs (- (improve guess x) guess)) 
-     (* guess .001))) 
+(define (good-enough? guess prev-guess)
+  (< (/ (abs (- guess prev-guess))
+        guess)
+     0.001))
 
-(define (sqrt-iter guess oldguess x) 
-   (if (good-enough? guess oldguess) 
-       guess 
-       (sqrt-iter (improve guess x) guess 
-                  x))) 
-  
-(define (good-enough? guess oldguess) 
-   (< (abs (- guess oldguess)) 
-      (* guess 0.001))) 
-  
+(define (sqrt-iter guess prev-guess x)
+  (if (good-enough? guess prev-guess)
+    guess
+    (sqrt-iter (improve guess x)
+               guess
+               x)))
+
+(define (square x)
+  (* x x)
+  )
+
 (define (sqrt x) 
-   (sqrt-iter 1.0 2.0 x))
+   (sqrt-iter 1.0 0 x))
 
-(sqrt 0.00001)
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2)
+  )
+
+(sqrt 0.000001)
+(sqrt 0.00000001)
+(sqrt 0.0000000001)
+
+(sqrt 1000000000000)
+(sqrt 100000000000000)
+
