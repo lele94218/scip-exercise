c8cd1b2ae8bd262f1f7c899b9c7ce909add9a55f
lele94218 <terry.trxue@gmail.com>
2016-12-23 18:37:50 -0500

    chp_1

---
+++ 1-22.lisp
@@ -0,0 +1,50 @@
+(define runtime real-time-clock)
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (runtime) start-time))))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (find-divisor n test-divisor)
+	(cond 
+		((> (square test-divisor) n) n)
+		((divides? test-divisor n) test-divisor)
+		(else (find-divisor n (+ test-divisor 1)))
+	)
+)
+
+(define (square x)
+	(* x x)
+)
+
+(define (divides? a b)
+	(= (remainder b a) 0)
+)
+
+(define (smallest-divisor n)
+	(find-divisor n 2)
+)
+
+(define (prime? n)
+	(= n (smallest-divisor n))
+)
+
+(define (search-for-primes start end)
+	(if (even? start)
+		(search-for-primes (+ start 1) end)
+		(cond 
+			((< start end) (timed-prime-test start)
+			(search-for-primes (+ start 2) end))
+		)
+	)
+)
+
+(search-for-primes 1000000 1000100)
